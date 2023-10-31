85a5c92803ce43f52eb55807d79948ba81c3a63e
Kunal Walavalkar <110326359+Knign@users.noreply.github.com>
2023-10-31 13:40:57 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-with st.sidebar:
-  selected = option_menu(
-    menu_title=None,
-    options = ["Home", "Affine", "Base64", "Caesar", "Railfence", "Reverse"],
-    icons = ["house", "shield-lock", "shield-lock", "shield-lock", "shield-lock", "shield-lock"]
-  )
+
 
 st.title('D|Cipher',anchor=False)
 st.subheader('Web-based toolkit that uses Python to encrypt and decrypt your text.')
